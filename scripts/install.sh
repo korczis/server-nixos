233f5a4167f2808b2352c04b62a5e73ec0f2eff3
Tomas Korcak <korczis@gmail.com>
2019-02-03 21:55:25 +0100

    Improve install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,15 @@
 
 # Install prerequisites
 ./prerequisites.sh
+
+# FS
+# mkfs.ext4 -L nixos /dev/sda1
+# mkfs.fat -F 32 -n boot /dev/disk/
+
+# mounts
+mount /dev/disk/by-label/nixos /mnt
+mount /dev/disk/by-label/boot /mnt/boot
+
+# NixOS
+nixos-generate-config --root /mnt
+nixos-install
