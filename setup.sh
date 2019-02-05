5cbd4d202c3b09d03b667f5a3e68636270c4a1f8
Tomas Korcak <korczis@gmail.com>
2019-02-06 00:46:29 +0100

    Improve setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,33 @@
 #! /usr/bin/env sh
 
+SERVER_NIXOS_ROOT="~/.server-nixos"
+
+# ----------------
+
+echo ""
 echo "-----------"
 echo "NixOS setup"
 echo "-----------"
 
-echo ""
+# -------------------
 
+echo ""
+echo "--------------"
 echo "Installing git"
+echo "--------------"
+
 nix-env -iAv nixos.pkgs.gitAndTools.git
+
+echo ""
+echo "--------------"
+echo "Installing OpenSSH server"
+echo "--------------"
+
+# nix-env -iAv nixos.pkgs.
+
+if [ ! -d "${SERVER_NIXOS_ROOT}" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  echo "Creating "${SERVER_NIXOS_ROOT}"
+fi
+
+# git clone https://github.com/korczis/server-nixos ${SERVER_NIXOS_ROOT}
